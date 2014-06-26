91c5a1f9f2d9650812a7db82972226c7d05f412d
markus <markus@namaniatsea.org>
2014-06-26 09:44:54 -0400

    fixed error in pook.py

--- pook.py
+++ pook.py
@@ -2,6 +2,7 @@ import os
 import web
 import threading
 from alyosha import alyosha as al
+from alyosha import reference as REF
 
 render = web.template.render('templates/')
 
@@ -16,7 +17,9 @@ class index:
         result = {}
         if not qp.q:
             return render.index(result)
-        result = al.full_results(al.SOURCE_SITES, qp.q, max_links=3)
+        source_sites = dict((key, value[0]) for (key, value) in
+                            REF.source_sites.iteritems())
+        result = al.full_results(source_sites, qp.q, max_links=3)
         return render.index(result)
 
 # if __name__ == '__main__':
