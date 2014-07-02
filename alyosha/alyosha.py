3684435a075c9ba29ba7a8547698fcfc9a334d3a
markus <markus@namaniatsea.org>
2014-07-01 21:48:48 -0400

    WIP: refactored wcounts into WebArticle class

--- alyosha/alyosha.py
+++ alyosha/alyosha.py
@@ -41,6 +41,155 @@ class ResultParsingError(Exception):
     pass
 
 
+class WebArticle(object):
+    """
+    Will retrieve aN URL, extract the main article and expose word/phrase lists
+    and counts.
+
+    Attributes:
+    -----------
+        url : str
+            URL from which instance was constructed
+        title : str
+            Title of article as provided by Goose().extract()
+        text : str
+            Cleaned text of article
+        wcount : int
+            Number of words in cleaned text
+        top_words : list
+            Unique list of words, sorted by descending frequency
+        top_phrases : list
+            Unique list of multi-word phrases, sorted first by length of
+            phrase, then by frequency
+        phrase_overlaps : list
+            List of single words in top_words that also appear as part of a
+            phrase in top_phrases
+        pruned_top_words : list
+            top_words - phrase_overlaps, with original order of top_words
+            retained
+    """
+    def __init__(self, ref_url, stop_words=None, late_kills=None):
+        """
+        Arguments:
+        ----------
+        ref_url : str
+            Web page from which search terms are to be extracted
+        stop_words : sequence or set
+            List or set with common words to be excluded from search string.
+            `stop_list` will be applied *before* any multiple word phrases are
+            constructed.
+        late_kills : sequence or set
+            Like `stop_words` but the words in `late_kills` will only be
+            eliminated from the search string *after* multiple word phrases
+            have been constructed. This you can have a word like 'report'
+            appear in the search string as part of a multiple word phrase
+            ("OECD Report on Public Health") but not as a single word (which
+            would have almost zero selectivity for a news article.
+        """
+        if not late_kills:
+            late_kills = []
+
+        self.url = ref_url
+        result = requests.get(ref_url, headers={'User-Agent':
+                random.choice(REF.user_agents)}, proxies=_get_proxies())
+        article = Goose().extract(raw_html=result.text)
+        self.title = article.title
+        self.text = article.cleaned_text
+
+        def build_wlist(raw_text, stop_words):
+            raw_text = unicodedata.normalize(
+                    'NFKC', raw_text)
+            if not stop_words:
+                stop_words = []
+            # strip punctuation to build word list excluding stop_list
+            raw_text = re.sub(ur'[\u2019]+', u'\'', raw_text)
+            raw_text = re.sub(ur'\'s', '', raw_text)
+            raw_text = re.sub(ur'n\'t', '', raw_text)
+            wlist = re.findall(ur'\w+', raw_text)
+            fwcount = len(wlist)
+            wlist = [w for w in wlist if w not in stop_words]
+            return wlist, fwcount
+
+        wlist, self.wcount = build_wlist(self.text, stop_words)
+        logging.debug("built %d word list for article \"%s\"" %
+                (len(wlist), self.title))
+
+        def phrase_counts(word_list, phrase_length=2, min_count=0):
+            """
+            Looks for multiple word phrases in text.  Takes `word_list` and
+            checks for repeated occurence of multiple word phrases (e.g.
+            'snowden files').  Returns a list of tuples (phrase, count), sorted
+            by count in descending order.  `phrase_length` indicates how many
+            words whould be in a phrase.  If `min_count` > 0 only phrases that
+            occur at least `min_count` times will be returned.
+            """
+            assert 1 <= phrase_length <= len(word_list)
+            cols = []
+            for i in range(phrase_length):
+                cols.append(word_list[i:])
+            phrases = ([' '.join(c) for c in zip(*cols)])
+            top_list = Counter(phrases).most_common()
+            top_list = [t for t in top_list if t[1] >= min_count]
+            return top_list
+
+        # get word/phrase counts:
+        min_count = max(2, int(round(len(wlist)/100.)))
+        search_term_list = []
+        for i in range(len(wlist)):
+            # frequency threshold for phrases only:
+            m = min_count if i > 0 else 0
+            terms = phrase_counts(wlist, i + 1, m)
+            if not terms:
+                break
+            search_term_list.append([t[0] for t in terms])
+
+        # keep full word search list:
+        self.top_words = [w for w in search_term_list[0]
+                          if w not in late_kills]
+        # eliminate redundancies:
+        pruned_list = []
+        for short_terms, long_terms in zip(search_term_list[:-1],
+                                           search_term_list[1:]):
+            short_terms = [t for t in short_terms
+                           if not t in ' '.join(long_terms)]
+            pruned_list.append(short_terms)
+        pruned_list.append(search_term_list[-1])
+
+        phrases = []
+        # iterating in reverse order to put multiple word phrases first
+        # NOTE: reversing no longer required
+        first_element = len(pruned_list) - 1
+        for i, terms in enumerate(reversed(pruned_list)):
+            # kill weak single words and enclose multiple word phrases in
+            # double quotes:
+            if i == first_element:
+                pass
+                # pruned_words no longer stored - use pruned_top_words property
+                # instead
+                # pruned_words = [t for t in terms if t not in late_kills]
+            else:
+                phrases += ['"%s"' % t for t in terms]
+
+        self.top_phrases = phrases
+
+    @property
+    def phrase_overlaps(self):
+        """
+        Provides a list of the words in self.top_words that are also part of a
+        multi-word phrase in self.top_phrases
+        """
+        return [w for w in self.top_words if w in ' '.join(self.top_phrases)]
+
+    @property
+    def pruned_top_words(self):
+        """
+        Same as self.top_words - self.phrase_overlaps, retaining original order
+        of self.top_words
+        """
+        excludes = self.phrase_overlaps
+        return [w for w in self.top_words if not w in excludes]
+
+
 class SiteResults(object):
     """
     Container for search results.
@@ -71,8 +220,8 @@ class SiteResults(object):
     # as `href` attributes: need multiple xp's to collect news items at top of
     # results and regular results
     _xp_title = [
-                 # _prefix + '//span[@class="tl"]/a[1]',
-                 _prefix + '//h3[@class="r"]/a[1]'
+            # _prefix + '//span[@class="tl"]/a[1]',
+            _prefix + '//h3[@class="r"]/a[1]'
     ]
     _xp_link = _prefix + '//cite'
     _xp_desc = _prefix + '//div[@class="s"]//span[@class="st"]'
@@ -149,109 +298,3 @@ def full_results(source_sites, query):
             continue
 
     return res
-
-
-def build_search_string(ref_url, stop_words=None, late_kills=None):
-    """
-    Analyses the page at ref_url to construct a string of search terms.
-
-    Arguments:
-    ----------
-    ref_url : str
-        Web page from which search terms are to be extracted
-    stop_words : sequence or set
-        List or set with common words to be excluded from search string.
-        `stop_list` will be applied *before* any multiple word phrases are
-        constructed.
-    late_kills : sequence or set
-        Like `stop_words` but the words in `late_kills` will only be
-        eliminated from the search string *after* multiple word phrases have
-        been constructed. This you can have a word like 'report' appear in
-        the search string as part of a multiple word phrase ("OECD Report on
-        Public Health") but not as a single word (which would have almost
-        zero selectivity for a news article.
-
-    Returns:
-    --------
-    Tuple wih three lists `(phrases, words, pruned_words`). `phrases` will
-    contain multiple word phrases, each enclosed in '"'.  `words` will contain
-    single words, some which may be duplicated as part of `phrases`.
-    `pruned_words` is like `words` but without any overlaps with `phrases`.
-    """
-
-    if not late_kills:
-        late_kills = []
-
-    result = requests.get(ref_url, headers={'User-Agent':
-            random.choice(REF.user_agents)}, proxies=_get_proxies())
-    article = Goose().extract(raw_html=result.text)
-    article.wlist = _build_wlist(article.cleaned_text, stop_words)
-    wcount = len(article.wlist)
-    logging.debug("built %d word list for article \"%s\"" % (wcount,
-                                                            article.title))
-    # get word/phrase counts:
-    min_count = max(2, int(round(wcount/100.)))
-    search_term_list = []
-    for i in range(len(article.wlist)):
-        # frequency threshold for phrases only:
-        m = min_count if i > 0 else 0
-        terms = _phrase_counts(article.wlist, i + 1, m)
-        if not terms:
-            break
-        search_term_list.append([t[0] for t in terms])
-
-    # keep full word search list:
-    full_words = [w for w in search_term_list[0] if w not in late_kills]
-    # eliminate redundancies:
-    pruned_list = []
-    for short_terms, long_terms in zip(search_term_list[:-1],
-                                       search_term_list[1:]):
-        short_terms = [t for t in short_terms
-                       if not t in ' '.join(long_terms)]
-        pruned_list.append(short_terms)
-    pruned_list.append(search_term_list[-1])
-
-    phrases = []
-    # iterating in reverse order to put multiple word phrases first
-    first_element = len(pruned_list) - 1
-    for i, terms in enumerate(reversed(pruned_list)):
-        # kill weak single words and enclose multiple word phrases in double
-        # quotes:
-        if i == first_element:
-            pruned_words = [t for t in terms if t not in late_kills]
-        else:
-            phrases += ['"%s"' % t for t in terms]
-
-    return (phrases, full_words, pruned_words)
-
-
-def _phrase_counts(word_list, phrase_length=2, min_count=0):
-    """
-    Looks for multiple word phrases in text.  Takes `word_list` and checks
-    for repeated occurence of multiple word phrases (e.g. 'snowden files').
-    Returns a list of tuples (phrase, count), sorted by count in descending
-    order.  `phrase_length` indicates how many words whould be in a phrase.
-    If `min_count` > 0 only phrases that occur at least `min_count` times
-    will be returned.
-    """
-    assert 1 <= phrase_length <= len(word_list)
-    cols = []
-    for i in range(phrase_length):
-        cols.append(word_list[i:])
-    phrases = ([' '.join(c) for c in zip(*cols)])
-    top_list = Counter(phrases).most_common()
-    top_list = [t for t in top_list if t[1] >= min_count]
-    return top_list
-
-
-def _build_wlist(raw_text, stop_words):
-    raw_text = unicodedata.normalize(
-            'NFKC', raw_text)
-    if not stop_words:
-        stop_words = []
-    # strip punctuation to build word list excluding stop_list
-    raw_text = re.sub(ur'[\u2019]+', u'\'', raw_text)
-    raw_text = re.sub(ur'\'s', '', raw_text)
-    raw_text = re.sub(ur'n\'t', '', raw_text)
-    wlist = re.findall(ur'\w+', raw_text)
-    return [w for w in wlist if w not in stop_words]
