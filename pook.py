9afe1cefcd2c0898117b3cf889ba1869d8d23dae
markus <markus@namaniatsea.org>
2014-07-13 21:50:13 -0400

    pook now uses web.seeother

--- pook.py
+++ pook.py
@@ -1,11 +1,12 @@
+from urllib import urlencode
+from urlparse import urlparse
+import logging
 import web
 from web import form
-import logging
 from alyosha import alyosha as al
 from alyosha import reference as REF
 # TODO: add threading for web requests
 
-# Configure the logger
 logging.basicConfig(level=logging.DEBUG)
 
 render = web.template.render('templates/', base='layout')
@@ -19,11 +20,11 @@ urls = (
 )
 
 urlForm = form.Form(
-        form.Textbox('URL', form.notnull, decription='Reference URL'),
+        form.Textbox('URL', form.notnull, description='Reference URL'),
         class_='urlform')
 
 searchPhrasesForm = form.Form(
-        form.Checkbox('Search phrases'),
+        form.Checkbox('Use phrases'),
         form.Checkbox('Force phrases'))
 
 dateRangeForm = form.Form(
@@ -49,6 +50,13 @@ class index(object):
         return render.index('/request')
 
 
+class error(object):
+
+    def GET(self):
+        i = web.input(msg='', back_link='/index')
+        return render.error(i.msg, i.back_link)
+
+
 class request(object):
 
     def GET(self):
@@ -58,54 +66,70 @@ class request(object):
         for form in formDict.values():
             if not form.validates():
                 return render.request(formDict)
-
-        # extract form data
         form_data = web.input()
-        ref_url = form_data['URL']
-        months_in_past = int(form_data['Months in past'])
-        searchPhrases = 'Search phrases' in form_data
-        forcePhrases = 'Force phrases' in form_data
-        logging.debug("ref_url=%s" % ref_url)
-        logging.debug("month in past=%d" % months_in_past)
-        logging.debug("Search phrases=%s" % searchPhrases)
-        logging.debug("Force phrases=%s" % forcePhrases)
+        parsed = urlparse(form_data['URL'])
+        scheme = parsed[0] if parsed[0] else 'http'
+        # ignore any parameters url:
+        ref_url = scheme + '://' + parsed[1] + parsed[2]
+        back_days = int(form_data['Months in past']) * 30
+        use_phrases = 'Use phrases' in form_data
+        force_phrases = 'Force phrases' in form_data
+        params = {
+                'url': ref_url,
+                'use_phrases': use_phrases,
+                'force_phrases': force_phrases,
+                'back_days': back_days
+        }
+        logging.debug("request parameter: %s" % params)
+        raise web.seeother('/results?' + urlencode(params))
 
+
+class results(object):
+
+    def GET(self):
+        i = web.input(url=None, use_phrases=False, force_phrases=False,
+                back_days=None)
         try:
-            wa = al.WebArticle(ref_url, REF.stop_words,
-                    REF.late_kills)
+            wa = al.WebArticle(i.url, REF.stop_words, REF.late_kills)
             logging.debug("article '%s' (%d words) at url='%s': query='%s'",
                     wa.title, wa.wcount, wa.url,
-                    wa.search_string(use_phrases=searchPhrases,
-                        force_phrases=forcePhrases))
+                    wa.search_string(use_phrases=i.use_phrases,
+                        force_phrases=i.force_phrases))
         except al.ArticleFormatError:
-            return render.error("Non-html resource at %s" % ref_url,
-                                '/request')
+            params = {
+                    'msg': "Non-html resource at %s" % i.url,
+                    'back_link': '/request'
+            }
+            raise web.seeother('/error?' + urlencode(params))
         except al.ArticleExtractionError:
-            return render.error("Cannot extract article from %s" % ref_url,
-                                '/request')
+            params = {
+                    'msg': "Cannot extract article from %s" % i.url,
+                    'back_link': '/request'
+            }
+            raise web.seeother('/error?' + urlencode(params))
         except al.InvalidUrlError:
-            return render.error("Invalid URL: %s" % ref_url, '/request')
+            params = {
+                    'msg': "Invalid URL: %s" % i.url,
+                    'back_link': '/request'
+            }
+            raise web.seeother('/error?' + urlencode(params))
         except al.PageRetrievalError:
-            return render.error("Could not retrieve URL: %s" % ref_url,
-                                '/request')
+            params = {
+                    'msg': "Could not retrieve URL: %s" % i.url,
+                    'back_link': '/request'
+            }
+            raise web.seeother('/error?' + urlencode(params))
 
         results = {}
         for cat, rng in src_cats.iteritems():
             sources = dict((src, v[0]) for (src, v) in
                     REF.source_sites.iteritems() if rng[0] <= v[1] < rng[1])
             results[cat] = al.rank_matches(wa, sources,
-                    use_phrases=searchPhrases, force_phrases=forcePhrases,
-                    back_days=months_in_past * 30, allintext=True)
+                    use_phrases=i.use_phrases, force_phrases=i.force_phrases,
+                    back_days=int(i.back_days), allintext=False)
             logging.debug("%s: %d ranked results", cat, len(results[cat]))
 
         return render.results(wa, results, '/request')
-
-
-class results(object):
-
-    def GET(self):
-        return render.results(None, None,
-                REF.source_sites.keys(), '/request')
 
 
 # For serving using any wsgi server
