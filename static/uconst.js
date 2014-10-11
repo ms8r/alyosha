44bf6122289bd21142805941566ccd1dcaafcc03
markus <markus@namaniatsea.org>
2014-10-11 17:01:07 +0200

    under contruction message via js

---
+++ static/uconst.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    alert("Hi there - Thanks for stopping by. Please note that I have disabled the search functionality temporarily so I won't have to pay for idling server resources while development is running on a slow burn. All the static content is still accessible through the links at the top. If you're interested in taking Thify for a spin just drop me a line at ms [at] thify.org and I will switch search back on.");
+});
